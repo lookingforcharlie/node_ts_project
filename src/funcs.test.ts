f03111e0a3a95c43adcfa2da9ee981b1d840cb3d
lookingforcharlie <charliefeng2020@gmail.com>
2025-05-22 16:45:58 -0400

    added test cases

--- src/funcs.test.ts
+++ src/funcs.test.ts
@@ -2,11 +2,19 @@ import { describe, expect, it, test } from 'vitest'
 import { calculateWordValue, explosion, getLanguagesAbove60 } from './jsfuncs'
 
 describe('calculateWordValue', () => {
-  test('should return the correct value for the word "love"', () => {
+  it('should return the correct value for the word "love"', () => {
     expect(calculateWordValue('love')).toBe(54)
   })
 
   it('should return the correct value for the word "friendship"', () => {
     expect(calculateWordValue('friendship')).toBe(108)
   })
+
+  it('should return the correct value for the word "a"', () => {
+    expect(calculateWordValue('a')).toBe(1)
+  })
+
+  // it('this test should fail', () => {
+  //   expect(false).toBe(true)
+  // })
 })
